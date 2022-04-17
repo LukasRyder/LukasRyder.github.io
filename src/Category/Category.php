ac4adc6f3cc803a15087477b419ea7f3c907c293
Jan-Marten de Boer <janmarten@elgentos.nl>
2022-04-17 23:36:52 +0200

    Remove unused import

--- src/Category/Category.php
+++ src/Category/Category.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace LukasRyder\Notes\Category;
 
-use JetBrains\PhpStorm\Internal\TentativeType;
 use RecursiveIterator;
 
 final class Category implements ParentNodeInterface
