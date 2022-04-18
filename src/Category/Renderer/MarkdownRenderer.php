1da57232a4a2537e0debf21e5948304e56e7dfdf
Jan-Marten de Boer <janmarten@elgentos.nl>
2022-04-18 12:51:20 +0200

    Nest categories in collapsable <details>

--- src/Category/Renderer/MarkdownRenderer.php
+++ src/Category/Renderer/MarkdownRenderer.php
@@ -14,14 +14,12 @@ final class MarkdownRenderer implements RendererInterface
 {
     private const DEFAULT_PADDING_CHARACTER = ' ';
     private const DEFAULT_PADDING_SIZE = 2;
-    private const DEFAULT_LIST_ITEM_PREFIX = '- ';
     private const DEFAULT_ENCODE_SPACES = false;
     private const DEFAULT_HEADING_PREFIX = '#';
 
     public function __construct(
         private readonly string $paddingCharacter = self::DEFAULT_PADDING_CHARACTER,
         private readonly int $paddingSize = self::DEFAULT_PADDING_SIZE,
-        private readonly string $listItemPrefix = self::DEFAULT_LIST_ITEM_PREFIX,
         private readonly bool $encodeSpaces = self::DEFAULT_ENCODE_SPACES,
         private readonly string $headingPrefix = self::DEFAULT_HEADING_PREFIX
     ) {}
@@ -75,28 +73,38 @@ final class MarkdownRenderer implements RendererInterface
         );
     }
 
-    private function createPrefix(int $depth): string
-    {
-        return $this->createPadding($depth) . $this->listItemPrefix;
-    }
-
     private function renderCategory(Category $category, int $depth): Generator
     {
-        yield $this->createPrefix($depth) . $category->getName();
+        $padding = $this->createPadding($depth);
+        $contentPadding = $padding . $this->createPadding(1);
+
+        yield $padding . '<details>';
+
+        yield $contentPadding . sprintf(
+            '<summary>%s</summary>',
+            $category->getName()
+        );
+
+        yield $contentPadding . '<ul>';
 
         foreach ($category as $child) {
-            yield from $this->renderChild($child, $depth + 1);
+            yield from $this->renderChild($child, $depth + 2);
         }
+
+        yield $contentPadding . '</ul>';
+        yield $padding . '</details>';
     }
 
     private function renderNote(Note $note, int $depth): Generator
     {
-        yield $this->createPrefix($depth) . sprintf(
-            '[%s](%s)',
-                $note->getName(),
-                $this->encodeSpaces
-                    ? str_replace(' ', '%20', $note->file->getPathname())
-                    : $note->file->getPathname()
+        $url = $this->encodeSpaces
+            ? str_replace(' ', '%20', $note->file->getPathname())
+            : $note->file->getPathname();
+
+        yield $this->createPadding($depth) . sprintf(
+            '<li><a href="%s">%s</a></li>',
+            $url,
+            $note->getName()
         );
     }
 }
